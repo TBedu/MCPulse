19ede356d4c289f151f0a575884188d8f50328c8
LinYe2406 <2087075078@qq.com>
2025-07-20 22:44:41 +0800

    Update servers.ts

--- .vitepress/data/servers.ts
+++ .vitepress/data/servers.ts
@@ -40,4 +40,14 @@ export const servers: MinecraftServer[] = [
     link: 'https://wiki.tbedu.top',
     ip: 'mc.tbedu.top'
   },
+  {
+    id: '3',
+    name: 'BySide ',
+    type: '生存',
+    version: '1.21.1',
+    icon: '/img/default.jpg',
+    description: '拥有生电生存小游戏合集的我的世界高版本服务器，服务器建设中，持续更新中......',
+    link: 'https://qm.qq.com/q/S9wHA1N8IO',
+    ip: 'mc.byside.top'
+  },
 ]
